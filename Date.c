133a841f7b2cb15f72fc597ac3018126e7cb8bea
Dean Ahmad <deandeanahmad@gmail.com>
2016-12-20 09:00:38 +0800

    Create Date.c

---
+++ Date.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int mm,dd;
+		
+		printf("Enter month:");
+		scanf("%d",&mm);
+		
+		printf("enter day: ");
+		scanf("%d",&dd);
+		
+		if(mm==1){
+			printf("January");
+		}
+		if(mm==2){
+			printf("February");
+		}
+		if(mm==3){
+			printf("March");
+		}
+		if(mm==4){
+			printf("April");
+		}
+		if(mm==5){
+			printf("May");
+		}
+		if(mm==6){
+			printf("June");
+		}
+		if(mm==7){
+			printf("July");
+		}
+		if(mm==8){
+			printf("August");
+		}
+		if(mm==9){
+			printf("September");
+		}
+		if(mm==10){
+			printf("October");
+		}
+		if(mm==11){
+			printf("November");
+		}
+		if(mm==12){
+			printf("December");
+		}
+		
+		if(dd==1){
+			printf("Sunday");
+		}
+		if(dd==2){
+			printf("Monday");
+		}
+		if(dd==3){
+			printf("Tuesday");
+		}
+		if(dd==4){
+			printf("Wednesday");
+		}
+		if(dd==5){
+			printf("Thursday");
+		}
+		if(dd==6){
+			printf("Friday");
+		}
+		if(dd==7){
+			printf("Saturday");
+		}
+	
+}
