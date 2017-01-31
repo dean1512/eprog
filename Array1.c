37e6e91ea813512122fb6915edd850c987e4d996
Dean Ahmad <deandeanahmad@gmail.com>
2017-01-31 08:27:21 +0800

    Array1.c

--- Array1.c
+++ Array1.c
@@ -2,16 +2,16 @@
 
 int main(){
 	int i, grades[3];
-	float a, b;
+	float a, sum;
 	for(i=0; i<3; i++){ 
 		printf("Enter grade %d: ", i+1); 
 		scanf("%d", &grades[i]); 
 	} 
-	b=0; 
+	sum=0; 
 	for(i=0; i<3; i++){ 
-		b=b+grades[i]; 
+		sum=sum+grades[i]; 
 	} 
-	a=b/3; 
+	a=sum/3; 
 	printf("The grades ave: %.2f\n", a); 
 	if(a>=75){
 printf("Pass");
